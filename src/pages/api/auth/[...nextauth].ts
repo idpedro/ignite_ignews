81a7964139ba0f6b06c3d7c939aa40e842bb49f1
Pedro Ferreira <pedorferreira@ferreirasolucoes.site>
2021-06-04 06:22:24 -0300

    Save user to database on login

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,7 @@
 import NextAuth from "next-auth";
 import Providers from "next-auth/providers";
+import { query } from "faunadb";
+import { fauna } from "../../../services/fauna";
 
 export default NextAuth({
   providers: [
@@ -9,4 +11,28 @@ export default NextAuth({
       scope: "read:user",
     }),
   ],
+  callbacks: {
+    async signIn(user, account, profile) {
+      const { email } = user;
+      try {
+        await fauna.query(
+          query.If(
+            query.Not(
+              query.Exists(
+                query.Match(query.Index("user_by_email"), query.Casefold(email))
+              )
+            ),
+            query.Create(query.Collection("users"), { data: { email } }),
+            query.Get(
+              query.Match(query.Index("user_by_email"), query.Casefold(email))
+            )
+          )
+        );
+        console.log(user);
+        return true;
+      } catch {
+        return false;
+      }
+    },
+  },
 });
