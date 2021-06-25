d0157ef39105e94239dcdae960e7cd01e22d9129
Pedro Ferreira <pedorferreira@ferreirasolucoes.site>
2021-06-25 20:59:02 -0300

    Create Post page

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,7 +1,22 @@
+import { GetStaticProps } from "next";
+import Prismic from "@prismicio/client";
 import Head from "next/head";
+import Link from "next/link";
+import { getPrismicClient } from "../../services/prismic";
+import { RichText } from "prismic-dom";
 import styles from "./styles.module.scss";
 
-export default function Posts() {
+type post = {
+  slug: string;
+  title: string;
+  excerpt: string;
+  updatedAt: string;
+};
+interface PostsProps {
+  posts: post[];
+}
+
+export default function Posts({ posts }: PostsProps) {
   return (
     <>
       <Head>
@@ -9,35 +24,45 @@ export default function Posts() {
       </Head>
       <main className={styles.container}>
         <div className={styles.postList}>
-          <a>
-            <time>12 de Março de 2021</time>
-            <strong>Lorem ipsum dolor sit amet</strong>
-            <p>
-              Adipisicing elit. Tenetur ipsam reiciendis recusandae laudantium
-              laboriosam nesciunt quasi quae fugit nemo voluptatem, similique
-              corporis rerum culpa incidunt quis harum. Nam, quam dicta!
-            </p>
-          </a>
-          <a>
-            <time>12 de Março de 2021</time>
-            <strong>Lorem ipsum dolor sit amet</strong>
-            <p>
-              Adipisicing elit. Tenetur ipsam reiciendis recusandae laudantium
-              laboriosam nesciunt quasi quae fugit nemo voluptatem, similique
-              corporis rerum culpa incidunt quis harum. Nam, quam dicta!
-            </p>
-          </a>
-          <a>
-            <time>12 de Março de 2021</time>
-            <strong>Lorem ipsum dolor sit amet</strong>
-            <p>
-              Adipisicing elit. Tenetur ipsam reiciendis recusandae laudantium
-              laboriosam nesciunt quasi quae fugit nemo voluptatem, similique
-              corporis rerum culpa incidunt quis harum. Nam, quam dicta!
-            </p>
-          </a>
+          {posts.map((post) => (
+            <Link key={post.slug} href={`/posts/${post.slug}`}>
+              <a key={post.slug}>
+                <time>{post.updatedAt}</time>
+                <strong>{post.title}</strong>
+                <p>{post.excerpt}</p>
+              </a>
+            </Link>
+          ))}
         </div>
       </main>
     </>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+  const response = await prismic.query(
+    Prismic.Predicates.at("document.type", "post"),
+    {
+      fetch: ["post.title", "post.content"],
+      pageSize: 100,
+    }
+  );
+  const posts = response.results.map((post) => ({
+    slug: post.uid,
+    title: RichText.asText(post.data.title),
+    excerpt:
+      post.data.content.find((content) => content.type === "paragraph")?.text ??
+      "",
+    updatedAt: new Date(post.last_publication_date).toLocaleDateString(
+      "pt-BR",
+      { day: "2-digit", month: "long", year: "numeric" }
+    ),
+  }));
+
+  return {
+    props: {
+      posts,
+    },
+  };
+};
