1042fa1dfa17bee1b6ae2046db94f098a987190e
Pedro Ferreira <pedorferreira@ferreirasolucoes.site>
2021-06-25 21:00:20 -0300

    create componet to check the active link

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,19 @@
-import React from "react";
 import { SignInButton } from "../SignInButton";
+import { AtiveLink } from "../ActiveLink";
 import styles from "./styles.module.scss";
+
 export function Header() {
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="ig.news" />
         <nav>
-          <a className={styles.active}>Home</a>
-          <a href="">Posts</a>
+          <AtiveLink href="/" activeClassName={styles.active}>
+            <a>Home</a>
+          </AtiveLink>
+          <AtiveLink href="/posts" activeClassName={styles.active}>
+            <a>Posts</a>
+          </AtiveLink>
         </nav>
         <SignInButton />
       </div>
